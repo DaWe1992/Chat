3efd592a956347fb38deab4429bd854d373e9f9a
DaWe1992 <daniel.wehner92@gmx.de>
2017-03-08 10:25:20 +0100

    Add files via upload

---
+++ src/IHello.java
@@ -0,0 +1,13 @@
+package rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Interface IHello.
+ * @author Daniel
+ *
+ */
+public interface IHello extends Remote {
+	public String sayHello() throws RemoteException;
+}
